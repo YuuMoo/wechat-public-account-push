6c2e7109ae769a21a30515d24d082442a5d02239
YuuMoo <112839743+YuuMoo@users.noreply.github.com>
2022-09-05 14:42:16 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,33 +1,127 @@
 export const config = {
-    // 公众号配置
-    // 公众号appId
-    appId: "wxfd22566357ed3fb9",
-    // 公众号appSecret
-    appSecret: "7527e23d19122c6ea71d19e9383dd3ea",
-    // 模板消息id
-    templateId: "M25SSCo23rdwStCYuGWIndYZEYfZI_0zUotmES_JnIA	",
-    // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [],
+  /**
+   * 公众号配置
+   */
 
-    // 信息配置
-    // 所在省份
-    province: "河南",
-    // 所在城市
-    city: "焦作",
-    // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
-    birthdays: [
-      {"name": "小于", "year": "2000", "date": "04-07", "type": 'new'},
-    ],
-    // 相遇的日子，格式同上
-    loveDate: "2022-07-02",
+  // 公众号APP_ID
+  APP_ID: "wxfd22566357ed3fb9",
+
+  // 公众号APP_SECRET
+  APP_SECRET: "a8ad7ffdb661321202cd90d336da0496",
+
+  /**
+   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
+   * [
+   *  {
+   *    // 想要发送的人的名字
+   *    name: "老婆0",
+   *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+   *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
+   *    // 你想对他发送的模板消息的模板ID
+   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
+   *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+   *    openUrl: "https://wangxinleo.cn"
+   *   }, 
+   * ]
+   */
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: "小于",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "oFP-Z6LulZNrC3q3CwFStxGXj4yY",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "az6KCXFylstUvpVK_E4gO9MOTJcN70Xo8jDosKKddGw",
+      // 所在省份
+      province: "河南",
+      // 所在城市
+      city: "焦作",
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://weibo.com"
     }
+  ],
 
-// {{date.DATA}}  
-// 城市：{{city.DATA}}  
-// 天气：{{weather.DATA}}  
-// 最低气温: {{min_temperature.DATA}}  
-// 最高气温: {{max_temperature.DATA}}  
-// {{birthday_message.DATA}}
+  /**
+   * 回调消息 相关，主要用来展示发送是否成功/失败的数据
+   */
 
-// {{note_en.DATA}}  
-// {{note_ch.DATA}}
+  // 回调消息模板id, 用来看自己有没有发送成功的那个模板
+  CALLBACK_TEMPLATE_ID: "zjV1glIuCXo4Csr23aXWEio1iP74sZJ7IQEES95lDsg6c4yOU",
+
+  // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
+  CALLBACK_USERS: [
+    {
+      // 一般都填自己
+      name: "Yuu",
+      // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "oFP-Z6LulZNrC3q3CwFStxGXj4yY",
+    }, 
+  ],
+    
+  /**
+   * 信息配置
+   */
+
+  /** 天气相关 */
+
+  // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
+  PROVINCE: "河南",
+  // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
+  CITY: "焦作",
+
+  /** 重要节日相关 */
+
+  /**
+   * 重要节日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
+   * tpye必须填！ 只能 “生日” 和 “节日” 二选一!
+   * 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
+   * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
+   */
+  FESTIVALS: [
+    {"type": "生日", "name": "小于", "year": "2000", "date": "04-07"},
+  ],
+
+  /**
+   * 限制重要节日的展示条目, 需要填写数字; 
+   * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
+   * 如果为0, 则默认展示全部
+   */
+  FESTIVALS_LIMIT: 0,
+
+  /** 日期相关 */
+
+  /** 你现在可以随心增加你认为的所有的需要纪念的日子啦！
+    * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
+    * 比如：keyword: "love_date" ，在测试号中就是 {{ love_date.DATA }} 
+    * */
+  CUSTOMIZED_DATE_LIST: [
+    // 相遇的日子
+    {"keyword": "love_day", date: "2022-07-02"},
+  ],
+
+  /** 插槽 */
+
+  /** 你可以在这里写超多的你想显示的内容了！
+    * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
+    * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }} 
+    * */
+  SLOT_LIST: [
+    // 这样配置的话，就会每次发送这句话
+    {"keyword": "encourage_oneself", contents: "你主要的问题在于读书太少而想得太多"},
+    // 这样配置的话，就会每次随机选一句话发送
+    {"keyword": "lover_prattle", contents: [
+      "因为太喜欢你，所以看谁都像是情敌。",
+      "申请成为你爱里的永久居民。",
+      "你很傻，你很笨，可我还是很羡慕你，因为你有我",
+      "遇见你，就好像捡到了100斤的运气",
+    ]},
+  ],
+
+  /** 每日一言 */
+
+  // 好文节选的内容类型
+  // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
+  LITERARY_PREFERENCE: ""
+
+
+}
